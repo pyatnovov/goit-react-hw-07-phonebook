979bfc293feb3d2a004978cadac84201bf065355
Vova2580 <109892815+pyatnovov@users.noreply.github.com>
2023-04-04 23:36:36 +0300

    Додаю бекенд та отримую список контактів

--- src/redux/slice.js
+++ src/redux/slice.js
@@ -1,12 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-const contactsList = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+import { fetchContacts } from 'fetch';
+
+const contactsList = [fetchContacts(1)];
 const items = createSlice({
   name: 'items:',
   initialState: {
